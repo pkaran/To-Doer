28bc62b1bf12e7dc4fad3fb2209b44e9aa6c8213
Karan <karanpatel4567@gmail.com>
2015-09-20 12:04:54 -0700

    Added controller (with basic skeleteon) for TodoView.fxml

---
+++ ToDoController.java
@@ -0,0 +1,103 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import model.SubTask;
+import model.Task;
+import model.ToDoList;
+
+/**
+ * Controller for ToDoView.fxml
+ *
+ * @author Karan Patel
+ */
+
+public class ToDoController {
+
+    public void initialize(){
+
+    }
+
+    @FXML
+    private void showHideCompletedTask(){
+
+    }
+
+    @FXML
+    private void deleteTask(ActionEvent e){
+
+    }
+
+    @FXML
+    private Button clearDueDate;
+
+    @FXML
+    private Button saveNewTaskNameButton;
+
+    @FXML
+    private Label messageLabel;
+
+    @FXML
+    private VBox toDoListTasksInfoVBox;
+
+    @FXML
+    private ListView<ToDoList> todoListView;
+
+    @FXML
+    private Button addTodoListButton;
+
+    @FXML
+    private TextField newTaskTextField;
+
+    @FXML
+    private ListView<Task> incompleteTaskListView;
+
+    @FXML
+    private Button showHideCompletedTaskButton;
+
+    @FXML
+    private ListView<Task> completeTaskListView;
+
+    @FXML
+    private VBox taskInfoVBox;
+
+    @FXML
+    private TextField taskNameTextField;
+
+    @FXML
+    private DatePicker taskDueDatePicker;
+
+    @FXML
+    private ToggleButton lowPriorityToggle;
+
+    @FXML
+    private ToggleGroup priority_toggle_group;
+
+    @FXML
+    private ToggleButton mediumPriorityToggle;
+
+    @FXML
+    private ToggleButton highPriorityToggle;
+
+    @FXML
+    private ListView<SubTask> subTaskListView;
+
+    @FXML
+    private TextField addSubTaskField;
+
+    @FXML
+    private TextArea taskNoteTextArea;
+
+    @FXML
+    private Button addSubTaskButton;
+
+    @FXML
+    private Button addTaskButton;
+
+    @FXML
+    private Button deleteTaskButton;
+
+    @FXML
+    private TextField newToDoListTextField;
+
+}
